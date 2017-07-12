6ff6a8a33e63eb444bc5f4dd9c6ff49c05a0dcc5
Maria Paz <maria.pazrojasc@gmail.com>
2017-07-12 19:33:23 -0400

    actualizacion

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,31 @@
 $(document).ready(function(){
-	$(".btn").click(function(e){
+	$(".boton").click(function(e){
 		e.preventDefault();
 		var tarea = $("#tarea").val();
 		if (tarea == ""){
 			alert("Ingresa una tarea");
 		}else{
-			$("#to-do").append("<li><a href='#!' class='collection-item remove'>"+ tarea + "</a></li>");
+			$("#to-do").append("<li>"+
+									"<a href='#'' class='lista'>"+tarea+
+										"<a class='remove btn-flat'>"+
+											"<i class='fa fa-check' aria-hidden='true'></i>"+
+										"</a>"+
+										"<a class='done btn-flat'>"+
+											"<i class='fa fa-trash' aria-hidden='true'></i>"+
+										"</a>"+
+									"</a>"+
+								"</li>");
 			$("#tarea").val("");
+				$(".done").click(function(){
+			var rem = $(this).parent().remove();
+		});
+		$(".remove").click(function(){
+			var rem = $(this).parent().remove();
+			$("#done").append(rem);
+		});
 		}
-	})
-	$(document).on('click', '.remove', function(){
-		var rem = $(this).parent().remove();
-		$("#done").append(rem);
 	});
 });
 
 
-/*
 
-$("#to-do").click(function(e){
-		e.preventDefault();
-		var tarea = $("#tarea").val();
-		$(".enlace").remove();
-	})*/
